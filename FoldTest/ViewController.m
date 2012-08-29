6d16cf846713af62805df6ab267685eae7f1b043
Dana Smith <dana.smith@autodesk.com>
2012-08-29 11:42:42 -0400

    Initial checking

--- FoldTest/ViewController.m
+++ FoldTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "QuartzCore/CALayer.h"
 
 @interface ViewController ()
 
@@ -35,4 +36,52 @@
 	}
 }
 
+- (IBAction)flip:(id)sender
+{
+	UIView *flipView = [self.view viewWithTag:1];
+	
+	UIGraphicsBeginImageContext(flipView.bounds.size);
+	[flipView.layer renderInContext:UIGraphicsGetCurrentContext()];
+	
+	UIImageView *topView = [[UIImageView alloc] initWithFrame:flipView.frame];
+	topView.image = UIGraphicsGetImageFromCurrentImageContext();;
+	[self.view addSubview:topView];
+	
+	UIImageView *bottomView = [[UIImageView alloc] initWithFrame:flipView.frame];
+	bottomView.image = UIGraphicsGetImageFromCurrentImageContext();
+	[self.view addSubview:bottomView];
+	
+	UIGraphicsEndImageContext();
+
+	CGFloat angle;
+	if (flipView.hidden == NO)
+	{
+		flipView.hidden = YES;
+		angle = M_PI_2;
+	}
+	else 
+	{
+		topView.layer.transform = CATransform3DMakeRotation(M_PI_2, 0.0, 1.0, 0.0);
+		bottomView.layer.transform = CATransform3DMakeRotation(-M_PI_2, 0.0, 1.0, 0.0);
+		angle = 0;
+	}
+
+	[UIView animateWithDuration:2.0 animations:^{
+		CATransform3D xform = CATransform3DMakeRotation(angle, 0.0, 1.0, 0.0);
+		xform.m34 = 1.0/300.0;
+		topView.layer.transform = xform;
+		
+		xform = CATransform3DMakeRotation(-angle, 0.0, 1.0, 0.0);
+		xform.m34 = 1.0/300.0;
+		bottomView.layer.transform = xform;
+	} completion:^(BOOL finished) {
+		if (angle == 0)
+		{
+			flipView.hidden = NO;
+			
+		}
+		[topView removeFromSuperview];
+		[bottomView removeFromSuperview];
+	}];
+}
 @end
